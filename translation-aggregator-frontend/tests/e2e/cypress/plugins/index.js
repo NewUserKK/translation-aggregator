1b05a8bc43903a5aee0013ac3a26c87fa925c8e3
Konstantin Kostin <newuserkk@yandex-team.ru>
2021-02-26 17:43:05 +0300

    Restructure cypress tests

--- translation-aggregator-frontend/tests/e2e/cypress/plugins/index.js
+++ translation-aggregator-frontend/tests/e2e/cypress/plugins/index.js
@@ -19,16 +19,16 @@ module.exports = (on, config) => {
   on(
     "file:preprocessor",
     webpack({
-      webpackOptions: require("../../../webpack.config.js"),
+      webpackOptions: require("../../../../webpack.config.js"),
       watchOptions: {}
     })
   );
 
   return Object.assign({}, config, {
-    fixturesFolder: "tests/e2e/fixtures",
-    integrationFolder: "tests/e2e/specs",
-    screenshotsFolder: "tests/e2e/screenshots",
-    videosFolder: "tests/e2e/videos",
-    supportFile: "tests/e2e/support/index.js"
+    fixturesFolder: "tests/e2e/cypress/fixtures",
+    integrationFolder: "tests/e2e/cypress/specs",
+    screenshotsFolder: "tests/e2e/cypress/screenshots",
+    videosFolder: "tests/e2e/cypress/videos",
+    supportFile: "tests/e2e/cypress/support/index.js"
   });
 };
